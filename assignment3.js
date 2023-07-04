39015f4e36a76e7e2a53d6fb5a404d6e3f67a28f
reasonsharma123 <reasonsharma34@gmail.com>
2023-07-04 07:09:13 +0545

    3rd ass

---
+++ assignment3.js
@@ -0,0 +1,13 @@
+const cylinder = {
+     radius: 0,
+     height: 0,
+     calculateArea: function() {
+        const pi = Math.PI;
+        return pi * Math.pow(this.radius,2) * this.height;
+     }
+    };
+
+    cylinder.radius = 8;
+    cylinder.height=11;
+    console.log(cylinder.calculateArea());
+
